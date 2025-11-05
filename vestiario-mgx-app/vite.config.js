b98d27abab0b5a424224248be8b2b471f14e9ef3
gabrielelivigni15-cyber <gabrielelivigni15@gmail.com>
2025-11-05 22:42:41 +0100

    Update vite.config.js

--- vestiario-mgx-app/vite.config.js
+++ vestiario-mgx-app/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  preview: { port: 5173 },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
